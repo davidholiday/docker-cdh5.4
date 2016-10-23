31d1e36a0b545ee61db6b5f206f77fc0f1f3ae5b
David Holiday <dholiday@wintermute.local>
2016-10-23 10:36:59 -0600

    refactoring the metadata parsing to be less of a code smell

--- foxy/constants.py
+++ foxy/constants.py
@@ -5,4 +5,4 @@
 DEFAULT_DASH_NAME_PREFIX = "dockerdash for "
 DASH_NAME_ARG = "dashboard_name"
 CONTAINER_NAME_ARG = "container_name"
-TOP_LEVEL_METATDATA_FILTER = "dockerdash.c."
+TOP_LEVEL_METATDATA_FILTER = "foxy."
